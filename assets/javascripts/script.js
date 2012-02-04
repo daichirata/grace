687f409786eace9921ec886fc3e0b455cbd1e17b
Daic_h <bunny.hop.md@gmail.com>
2012-02-05 05:39:27 +0900

    add ui.totop and fonts

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -6,5 +6,7 @@ $(function(){
     $(".social-icon img").hover(function() {
         $(this).parent().next("span").animate({opacity: "show", top: "200"}, "slow");}, function(){
             $(this).parent().next("span").animate({opacity: "hide", top: "210"}, "fast");
-        });
+    });
+
+    $().UItoTop({ easingType: 'easeOutQuart' });
 });
--- helper/grace_helper.rb
+++ helper/grace_helper.rb
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 module Lokka
   module Helpers
+    def apply_continue_reading(posts)
+      posts.each do |post|
+        post.body.sub!(/<!-- ?more ?-->.*/m, %Q(<a class="read-more" href=\"#{post.link}\">#{I18n.t('continue_reading')}</a>))
+      end
+      posts
+    end
+
+    def grace_short_body
+      @short_body ||= self.long_body
+    end
   end
 end
 
