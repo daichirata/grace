747ebb76317956dff0fbae6f297cebb3c4ce143e
Dach-h <bunny.hop.md@gmail.com>
2012-02-06 12:36:41 +0900

    bugfix

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,7 +1,7 @@
 $(function(){
     $(".social-icon img").hover(function() {
-        $(this).parent().next("span").animate({opacity: "show", top: "190"}, "slow");}, function(){
-            $(this).parent().next("span").animate({opacity: "hide", top: "200"}, "fast");
+        $(this).parent().next("span").animate({opacity: "show", top: "170"}, "slow");}, function(){
+            $(this).parent().next("span").animate({opacity: "hide", top: "170"}, "fast");
     });
 
     $().UItoTop({ easingType: 'easeOutQuart' });
