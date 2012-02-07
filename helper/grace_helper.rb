0fe0632951c2d442cec3c4b9ac5a65a4fd61c42d
Daic_h <bunny.hop.md@gmail.com>
2012-02-08 03:52:28 +0900

    refactor

--- helper/grace_helper.rb
+++ helper/grace_helper.rb
@@ -104,6 +104,12 @@ module Lokka
       <script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
       HTML
     end
+
+    def grace_fork_me_on_github(user_name)
+      <<-HTML
+      <a href="http://github.com/#{user_name.to_s}"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/71eeaab9d563c2b3c590319b398dd35683265e85/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub"></a>
+      HTML
+    end
   end
 end
 
