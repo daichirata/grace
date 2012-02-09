36bd7e2146dad0e1671f74056b781ad8c65848a8
Dach-h <bunny.hop.md@gmail.com>
2012-02-09 16:03:47 +0900

    refactor

---
+++ assets/javascripts/pointer.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('body').pointer();
+});
+
+(function($){
+	$.fn.pointer = function (options) {
+		var settings = {
+			size : 80,
+			spd : 300,
+			color : "#ccc"
+		}
+		settings = $.extend(settings, options);
+		
+		var circle_style = {
+			"position":"absolute",
+			"z-index":9999,
+			"height":10,
+			"width":10,
+			"border":"solid 4px "+settings.color,
+			"border-radius":settings.size
+		}
+		return this.each(function() {
+			var $this = $(this);
+			$this.css({"position":"relative"});
+			$(document).click(function(e){
+				var x = e.pageX-5;
+				var y = e.pageY-5;
+				
+				var pos = {
+					top :(-settings.size/2)+y,
+					left :(-settings.size/2)+x
+				}
+		
+				$this.append('<div class="circle"></div>');
+				$this.find(".circle:last").css(circle_style).css({
+					"top":y,
+					"left":x
+				}).animate({"height":settings.size,"width":settings.size,"left":pos.left,"top":pos.top},{duration:settings.spd,queue:false})
+				.fadeOut(settings.spd*1.8,function(){
+					$(this).remove();
+				});
+			});
+		});
+	}
+})(jQuery); 
